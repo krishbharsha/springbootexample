639b1829863915ec0f683947469f1f5c356d3b33
Sharma <krishbharsha>
2018-03-25 22:36:35 -0700

    sample example

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,24 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        logger.info("greeting method called");
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
